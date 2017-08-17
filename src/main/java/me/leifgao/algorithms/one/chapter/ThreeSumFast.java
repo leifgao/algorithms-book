8abcba771073aa3d6dbae20b43fa3e1ad4bc9d07
LeifGao <leif@LeifGaos-MacBook-Pro.local>
2017-08-17 13:24:46 +0800

    threeSum快速算法

--- src/main/java/me/leifgao/algorithms/one/chapter/ThreeSumFast.java
+++ src/main/java/me/leifgao/algorithms/one/chapter/ThreeSumFast.java
@@ -4,27 +4,30 @@ import edu.princeton.cs.introcs.In;
 import edu.princeton.cs.introcs.StdOut;
 import edu.princeton.cs.introcs.Stopwatch;
 
+import java.util.Arrays;
+
 /**
- * Created by leif on 2017/8/16
+ * Created by leif on 2017/8/17
  */
-public class ThreeSum {
+public class ThreeSumFast {
+
     public static void main(String[] args) {
         int[] a = In.readInts(args[0]);
         Stopwatch timer = new Stopwatch();
         StdOut.println(count(a));
         double time = timer.elapsedTime();
-        System.out.println("普通算法时间:" + time);
+        System.out.println("fast算法时间:" + time);
     }
+
 
     public static int count(int[] a) {
         int N = a.length;
+        Arrays.sort(a);
         int cnt = 0;
         for (int i=0; i<N; i++) {
             for (int j=i+1; j<N; j++) {
-                for (int k=j+1; k<N; k++) {
-                    if (a[i] + a[j] + a[k] == 0) {
-                        cnt++;
-                    }
+                if (BinarySearch.rank(-a[i]-a[j], a) > j) {
+                    cnt++;
                 }
             }
         }
