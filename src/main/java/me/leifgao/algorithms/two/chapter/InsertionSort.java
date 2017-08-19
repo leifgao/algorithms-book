c0205d3386bbe5a248d0934e443df908091b719d
LeifGao <leif@LeifGaos-MacBook-Pro.local>
2017-08-19 12:15:13 +0800

    插入排序的实现

---
+++ src/main/java/me/leifgao/algorithms/two/chapter/InsertionSort.java
@@ -0,0 +1,27 @@
+package me.leifgao.algorithms.two.chapter;
+
+/**
+ * Created by leif on 2017/8/19
+ */
+public class InsertionSort extends AbstractSortExample{
+
+
+    @Override
+    public void sort(Comparable[] a) {
+        int N = a.length;
+
+        for (int i=1; i<N; i++) {
+            for (int j =i; j >0 && less(a[j], a[j-1]); j--) {
+                exch(a, j, j-1);
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        InsertionSort insertionSort = new InsertionSort();
+
+
+
+    }
+}
