3d113b8c1aa68e536d32aa559b29c57ad30e7c8e
LeifGao <leif@LeifGaos-MacBook-Pro.local>
2017-08-23 11:01:41 +0800

    无序符号表实现(节点链表实现),有序符号表实现(数组实现)

---
+++ src/main/java/me/leifgao/algorithms/three/chapter/BinarySearchST.java
@@ -0,0 +1,107 @@
+package me.leifgao.algorithms.three.chapter;
+
+import java.security.Key;
+
+/**
+ * Created by leif on 2017/8/23
+ */
+public class BinarySearchST<K extends Comparable<K>, V>{
+
+    private K[] keys;
+    private V[] values;
+    private int N;
+
+    public BinarySearchST(int capacity) {
+        keys = (K[]) new Comparable[capacity];
+        values = (V[]) new Object[capacity];
+    }
+
+    public int size() {
+        return N;
+    }
+
+    public boolean isEmpty() {
+        return N == 0 ;
+    }
+
+    public V get(K key) {
+        if (isEmpty()) {
+            return null;
+        }
+        int i = rank(key);
+        if (i < N && keys[i].compareTo(key) == 0) {
+            return values[i];
+        } else {
+            return null;
+        }
+    }
+
+    public void put(K key, V value) {
+        int i = rank(key);
+        if (i < N && keys[i].compareTo(key) == 0) {
+            values[i] = value;
+            return;
+        }
+        for (int j=N; j>i; j--) {
+            keys[j] = keys[j-1];
+            values[j] = values[j-1];
+        }
+        keys[i] = key;
+        values[i] = value;
+        N++;
+    }
+
+    public void delete(K key) {
+        int i = rank(key);
+        if (i<N && keys[i].compareTo(key) == 0) {
+            if (i<N && keys[i].compareTo(key) == 0) {
+                for (int j = i; j<N-1; j++) {
+                    keys[i] = keys[i+1];
+                    values[i] = values[i+1];
+                }
+                keys[N-1] = null;
+                values[N-1] = null;
+            }
+        }
+    }
+
+    /**
+     * 递归的二分查找
+     * @param key
+     * @param low
+     * @param high
+     * @return
+     */
+    public int rank(K key, int low, int high) {
+        if (high < low) {
+            return low;
+        }
+        int mid = low + (high - low)/2;
+        int cmp = key.compareTo(keys[mid]);
+        if (cmp < 0) {
+            return rank(key, low, mid -1);
+        } else if (cmp > 0) {
+            return rank(key, mid + 1, high);
+        } else {
+            return mid;
+        }
+    }
+
+    public int rank(K key) {
+        int low = 0;
+        int high = N-1;
+        while (low <= high) {
+            int mid = low + (high - low)/2;
+            int cmp = key.compareTo(keys[mid]);
+
+            if (cmp < 0) {
+                high = mid -1;
+            } else if (cmp > 0) {
+                low = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return low;
+    }
+}
---
+++ src/main/java/me/leifgao/algorithms/three/chapter/SequentialSearchST.java
@@ -0,0 +1,82 @@
+package me.leifgao.algorithms.three.chapter;
+
+/**
+ * Created by leif on 2017/8/22
+ */
+public class SequentialSearchST<K, V> {
+
+    private Node first;
+
+    public static void main(String[] args) {
+        SequentialSearchST<String, Integer> sst = new SequentialSearchST<>();
+        sst.put("s",0);
+        sst.put("e",1);
+        sst.put("a",2);
+        sst.put("r",3);
+        sst.printSST();
+        sst.delete("e");
+        sst.printSST();
+
+    }
+
+    private class Node {
+        K key;
+        V value;
+        Node next;
+
+        public Node(K key, V value, Node next) {
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
+    }
+
+    public V get(K key) {
+        for (Node n=first; n!= null; n=n.next) {
+            if (key.equals(n.key)) {
+                return n.value;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * 顺序插入 <k,v>, 当key相同时更新key对应的键值,否则新建节点
+     * @param key
+     * @param value
+     */
+    public void put(K key, V value) {
+        for (Node n=first; n!=null; n=n.next) {
+            if (key.equals(n.key)) {
+                n.value = value;
+                return;
+            }
+        }
+        first = new Node(key, value, first);
+    }
+
+    public void delete(K key) {
+        if (key == null) {
+            throw new IllegalArgumentException("delete key is null");
+        }
+        first = delete(first, key);
+    }
+
+    public Node delete(Node n, K key) {
+        if (n == null) {
+            return null;
+        }
+        if (key.equals(n.key)) {
+            return n.next;
+        }
+        n.next = delete(n.next, key);
+        return n;
+    }
+
+    private void printSST() {
+        for (Node n=first; n!=null; n=n.next) {
+            System.out.print("|"+n.key+":"+n.value + "|" + (n.next == null ? "\n" : "--->"));
+        }
+    }
+
+}
